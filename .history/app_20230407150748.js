f919c5f53facdf321aacd39f0d878e962f796c6d
unknown <pt06@firstins.com.tw>
2023-04-07 15:28:21 +0800

    'test'

--- .history/app_20230407150748.js
+++ .history/app_20230407150748.js
@@ -1,9 +1,6 @@
 const express = require('express')
 const app = express()
 const port = 3001
-const cors = require('cors');
-
-app.use(cors());
 
 app.get('/hello', (req, res) => {
   res.send('Hello World!!!!!')
