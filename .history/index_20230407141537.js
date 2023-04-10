727b8af147b3f4c1e7366f7a9ee5cf049000d419
unknown <pt06@firstins.com.tw>
2023-04-10 14:19:15 +0800

    'test'

--- .history/index_20230407141537.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql’);
-
-var conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'nodejs',
-    password: 'nodejs',
-    database:'demo_nodejs',
-    port: 3306
-});
--- .history/index_20230407141617.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-
-var conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'adminroot',
-    database:'test',
-    port: 3306
-});
