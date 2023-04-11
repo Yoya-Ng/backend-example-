9f821412d79aca50eab685bb96f3fc79c02a61a0
yo <unrealboundary@gmail.com>
2023-04-11 21:27:41 +0800

    test

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ pool.getConnection((err, connection) => {
 
 
 // // 引入 express 並使用
-// const express = require('express');
+const express = require('express');
 const app = express();
 // const cors = require('cors');
 // app.use(cors());
