48d0074a1b17385c6a5d902363c927e29924d321
unknown <pt06@firstins.com.tw>
2023-04-27 09:19:07 +0800

    'test'

--- app.js
+++ app.js
@@ -79,9 +79,10 @@ app.put('/users', function (req, res) {
 });
 
 app.post('/users', function (req, res) {
-    console.log(req);
-    console.log(req.body);
-
+  console.log(req);
+  console.log(req.body);
+  console.log(res);
+  console.log(res.body);
   res.json(req.body);
 // 接上連接池
 // pool.getConnection((err, connection) => {
